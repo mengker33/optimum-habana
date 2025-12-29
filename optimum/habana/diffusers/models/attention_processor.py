ddd725d6de045b3e50dddd1fb1c54353af513e5f
Wei Lin <wei2.lin@intel.com>
2025-12-29 14:06:44 +0800

    Update condition for Flash Attention V3

--- optimum/habana/diffusers/models/attention_processor.py
+++ optimum/habana/diffusers/models/attention_processor.py
@@ -117,7 +117,7 @@ class FlashAttnV3Gaudi:
         key_len = key.size(-2)
 
         # In the case of cross-attn, use FusedSDPA.
-        if (query_len * cp_size) != key_len or query_len <= 8192:
+        if (query_len * cp_size) != key_len or (query_len <= 8192 and key_len <= 8192):
             output = FusedSDPA.apply(query, key, value, attention_mask, 0.0, False, None, fsdpa_mode, None)
             return output.permute(0, 2, 1, 3).contiguous()
 
